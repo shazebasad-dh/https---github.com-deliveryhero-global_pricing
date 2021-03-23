77ec81c5c495f174f35a82ce0ed952e6dd1f82f7
Yuzhu Zhang <yuzhu.zhang@outlook.de>
2021-03-23 09:47:58 +0100

    Create delivery_costs.sql

---
+++ sql_script/delivery_costs.sql
@@ -0,0 +1,32 @@
+-- Date:            2021/03/23
+-- Contributor:     Yuzhu Zhang
+-- Enviroment:      Bigquery
+-- BI consultant:   Xun, Trupti
+-- Comment:    
+
+-- Xun's approach
+select
+  entity_id,
+  country_code,
+  created_date,
+  order_id,
+  sum(delivery_costs)
+from fulfillment-dwh-production.cl.utr_timings
+
+-- Laurent's approach
+select
+    p.entity_id,
+    l.platform_order_code order_code,
+    sum(p.delivery_costs) delivery_costs,
+  from 
+    (select
+      entity_id,
+      country_code,
+      created_date,
+      order_id,
+      delivery_costs,
+      row_number() over(partition by entity_id, order_id order by created_date desc) as rank
+    from fulfillment-dwh-production.cl.utr_timings) p
+  left join fulfillment-dwh-production.cl.orders l on p.order_id = l.order_id and p.country_code = l.country_code
+  where rank = 1
+  group by 1,2
