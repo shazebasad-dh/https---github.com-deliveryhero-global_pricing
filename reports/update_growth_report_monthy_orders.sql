b30a174a25166dedd8adc790a36a8bd5e27cc8dc
Laurent W. Broering <laurent.broering@deliveryhero.com>
2022-07-04 14:44:59 +0200

    Update update_growth_report_monthy_orders.sql

--- reports/update_growth_report_monthy_orders.sql
+++ reports/update_growth_report_monthy_orders.sql
@@ -1,3 +1,11 @@
+create or replace table pricing.growth_report_monthy_orders_kpis 
+partition by month
+options(
+  partition_expiration_days=null,
+  require_partition_filter=false
+)
+as 
+(
 select * from `dh-logistics-product-ops.pricing.growth_report_monthy_orders_kpis`
 where
   month < date_sub(date_trunc(current_date(), month), interval 1 month)
@@ -8,3 +16,4 @@ select *
 from `dh-logistics-product-ops.pricing.growth_report_monthy_orders_kpis_view`
 where
   month >= date_sub(date_trunc(current_date(), month), interval 1 month)
+)
