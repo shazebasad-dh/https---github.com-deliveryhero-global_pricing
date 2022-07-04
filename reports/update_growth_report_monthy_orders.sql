1d78fa6de88325e40bbd89c1057333b207d76990
Laurent W. Broering <laurent.broering@deliveryhero.com>
2022-07-04 14:59:37 +0200

    prettifying

--- reports/update_growth_report_monthy_orders.sql
+++ reports/update_growth_report_monthy_orders.sql
@@ -9,9 +9,7 @@ as
 select * from `dh-logistics-product-ops.pricing.growth_report_monthy_orders_kpis`
 where
   month < date_sub(date_trunc(current_date(), month), interval 1 month)
-
 union all
-
 select * 
 from `dh-logistics-product-ops.pricing.growth_report_monthy_orders_kpis_view`
 where
