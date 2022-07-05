ecc7cec207bad87227513f932ff6a958a8e32cc9
Laurent W. Broering <laurent.broering@deliveryhero.com>
2022-07-05 18:25:48 +0200

    Update update_pricing_reports.sql

--- reports/update_pricing_reports.sql
+++ reports/update_pricing_reports.sql
@@ -1,3 +1,5 @@
+-- growth_report_monthy_orders_kpis
+
 create or replace table pricing.growth_report_monthy_orders_kpis 
 partition by month
 options(
@@ -14,4 +16,44 @@ select *
 from `dh-logistics-product-ops.pricing.growth_report_monthy_orders_kpis_view`
 where
   month >= date_sub(date_trunc(current_date(), month), interval 1 month)
+);
+
+-- order_service_fee
+
+create or replace table pricing.order_service_fee 
+partition by created_date
+options(
+  partition_expiration_days=null,
+  require_partition_filter=false
 )
+as 
+(
+select * from `dh-logistics-product-ops.pricing.order_service_fee`
+where
+  month < date_sub(date_trunc(current_date(), month), interval 1 month)
+union all
+select * 
+from `dh-logistics-product-ops.pricing.order_service_fee_view`
+where
+  month >= date_sub(date_trunc(current_date(), month), interval 1 month)
+);
+
+-- delivery_costs_per_order
+
+create or replace table pricing.delivery_costs_per_order 
+partition by created_date
+options(
+  partition_expiration_days=null,
+  require_partition_filter=false
+)
+as 
+(
+select * from `dh-logistics-product-ops.pricing.delivery_costs_per_order`
+where
+  created_date < date_sub(date_trunc(current_date(), day), interval 15 day)
+union all
+select * 
+from `dh-logistics-product-ops.pricing.delivery_costs_per_order_view`
+where
+  created_date >= date_sub(date_trunc(current_date(), day), interval 15 day)
+);
