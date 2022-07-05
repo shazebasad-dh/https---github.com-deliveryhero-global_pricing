a73cf6094c4f52930e45acb89548452155096d81
Laurent W. Broering <laurent.broering@deliveryhero.com>
2022-07-05 18:33:10 +0200

    add projects

--- reports/update_pricing_reports.sql
+++ reports/update_pricing_reports.sql
@@ -1,6 +1,6 @@
 -- growth_report_monthy_orders_kpis
 
-create or replace table pricing.growth_report_monthy_orders_kpis 
+create or replace table `dh-logistics-product-ops.pricing.growth_report_monthy_orders_kpis`
 partition by month
 options(
   partition_expiration_days=null,
@@ -20,7 +20,7 @@ where
 
 -- order_service_fee
 
-create or replace table pricing.order_service_fee 
+create or replace table `dh-logistics-product-ops.pricing.order_service_fee`
 partition by created_date
 options(
   partition_expiration_days=null,
@@ -30,17 +30,17 @@ as
 (
 select * from `dh-logistics-product-ops.pricing.order_service_fee`
 where
-  month < date_sub(date_trunc(current_date(), month), interval 1 month)
+  created_date < date_sub(current_date(), interval 7 day)
 union all
 select * 
 from `dh-logistics-product-ops.pricing.order_service_fee_view`
 where
-  month >= date_sub(date_trunc(current_date(), month), interval 1 month)
+  created_date >= date_sub(current_date(), interval 7 day)
 );
 
 -- delivery_costs_per_order
 
-create or replace table pricing.delivery_costs_per_order 
+create or replace table `dh-logistics-product-ops.pricing.delivery_costs_per_order`
 partition by created_date
 options(
   partition_expiration_days=null,
