ee2309681749a5a9ce1e96728c79a8437b9e4820
Laurent W. Broering <laurent.broering@deliveryhero.com>
2022-07-05 11:30:38 +0200

    move service fee to data fridge again
    service fee in cDWH not fully available before deployment in June
    
    https://jira.deliveryhero.com/browse/CDWH-516?focusedCommentId=4615283&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-4615283

--- reports/growth_report_monthy_orders_kpis.sql
+++ reports/growth_report_monthy_orders_kpis.sql
@@ -1,9 +1,9 @@
 -- drop table pricing.growth_report_monthy_orders_kpis
-create or replace table pricing.growth_report_monthy_orders_kpis 
+create or replace table `dh-logistics-product-ops.pricing.growth_report_monthy_orders_kpis`
 partition by month
 options(
-  partition_expiration_days=null,
-  require_partition_filter=false
+  partition_expiration_days = null,
+  require_partition_filter = true
 )
 as 
 -- (
@@ -24,15 +24,15 @@ entities as (
     where e.is_reporting_enabled
         and e.is_platform_online
 )
--- , order_service_fee as (
---     select 
---         global_entity_id,
---         order_id,
---         customer.payment.service_fee sf,
---     from `fulfillment-dwh-production.curated_data_shared_data_stream.orders` as orders 
---     inner join entities using (global_entity_id)
---     where created_date between '2018-01-01' and current_date()
--- )
+, order_service_fee as (
+    select 
+        global_entity_id,
+        order_id,
+        customer.payment.service_fee sf,
+    from `fulfillment-dwh-production.curated_data_shared_data_stream.orders` as orders 
+    inner join entities using (global_entity_id)
+    where created_date between '2018-01-01' and current_date()
+)
 -- , delivery_costs as (
 --     select
 --         p.entity_id as global_entity_id,
@@ -61,8 +61,8 @@ select
     --sum(o.value.gbv_eur) gbv_eur,
     sum(o.value.mov_local) mov_local,
     --sum(o.value.mov_eur) mov_eur,
-    sum(o.value.service_fee_local) service_fee_local,
-    -- sum(sf) service_fee_local,
+    -- sum(o.value.service_fee_local) service_fee_local,
+    sum(sf) service_fee_local,
     sum(o.value.voucher_dh_local) voucher_dh_local,
     sum(o.value.discount_dh_local) discount_dh_local,
     sum(o.value.joker_vendor_fee_local) joker_vendor_fee_local,
@@ -71,14 +71,15 @@ select
     sum(case when o.value.delivery_fee_local = 0 then 1 else 0 end) orders_with_free_delivery,
     sum(case when o.value.mov_local > 0 then 1 else 0 end) orders_with_mov,
     sum(case when o.value.mov_customer_fee_local > 0 then 1 else 0 end) orders_with_mov_fee,
-    sum(case when o.value.service_fee_local > 0 then 1 else 0 end) orders_with_service_fee,
+    -- sum(case when o.value.service_fee_local > 0 then 1 else 0 end) orders_with_service_fee,
+    sum(case when sf > 0 then 1 else 0 end) orders_with_service_fee,
     sum(case when o.value.voucher_dh_local > 0 then 1 else 0 end) orders_with_dh_voucher,
     sum(case when o.value.discount_dh_local > 0 then 1 else 0 end) orders_with_dh_discount,
     sum(case when o.value.joker_vendor_fee_local > 0 then 1 else 0 end) orders_with_joker,
 from `fulfillment-dwh-production.curated_data_shared_central_dwh.orders` o
 inner join entities e using (global_entity_id)
 left join `fulfillment-dwh-production.curated_data_shared_central_dwh.vendors` v using (global_entity_id, vendor_id)
--- left join order_service_fee using (global_entity_id, order_id)
+left join order_service_fee using (global_entity_id, order_id)
 left join `dh-logistics-product-ops.pricing.delivery_costs_per_order` c using (global_entity_id, order_id)
 where o.is_sent
     and date(o.placed_at_local) between '2015-01-01' and current_date()
