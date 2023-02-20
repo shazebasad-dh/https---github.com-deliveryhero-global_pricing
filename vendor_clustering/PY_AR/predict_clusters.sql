e7627e83fc1613e744aa0dc230796d968d6b5954
Laurent W. Broering <laurent.broering@deliveryhero.com>
2023-02-20 14:15:33 +0100

    Update predict_clusters.sql

--- vendor_clustering/PY_AR/predict_clusters.sql
+++ vendor_clustering/PY_AR/predict_clusters.sql
@@ -2,7 +2,7 @@ select * except(nearest_centroids_distance)
 from ml.predict(
     model `dh-logistics-product-ops.pricing.vendor_clustering_model_ar_food_afv_distance_custom_c5`,
     (select * except (init_col)
-    from final_dataset
+    from `dh-logistics-product-ops.pricing.vendor_clustering_model_ar_food_afv_distance_data`
     where not init_col)
 )
 order by centroid_id
