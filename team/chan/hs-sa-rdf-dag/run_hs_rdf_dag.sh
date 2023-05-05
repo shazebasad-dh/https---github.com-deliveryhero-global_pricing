9dbf1ab370272349cd15b9a20a5e581568fd6a30
Sebastian Lafaurie <sebastian.lafaurie@deliveryhero.com>
2023-05-05 10:17:48 +0200

    update hs dag

--- team/chan/hs-sa-rdf-dag/run_hs_rdf_dag.sh
+++
@@ -1,3 +0,0 @@
-# #!/bin/sh
-/Users/s.lafaurie/opt/anaconda3/envs/eda_env/bin/python /Users/s.lafaurie/Documents/pricing-local/MENA/dag-hs-mapping-dps-session/dag.py -d 2
-echo "Cronjob ran successfully at $(date)" >> /var/log/hs_rdf_dag.log
