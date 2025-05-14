b185fcb16e0083e9bd87effd161543506d650b98
shazebasad-dh <shazeb.asad@deliveryhero.com>
2025-05-14 20:33:34 +0200

    created file to add functions that transform data by cleaning ( removing any nans) etc

---
+++ pricing_performance_holdouts/data/transform.py
@@ -0,0 +1,75 @@
+import logging
+logger = logging.getLogger(__name__)
+
+# ------------------------------------------------------------------------------
+# Data Clean Up
+# ------------------------------------------------------------------------------
+
+def fill_nans(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Replace NaN values in orders and profit columns with zeros.
+
+    Args:
+        df (pd.DataFrame): Input DataFrame.
+
+    Returns:
+        pd.DataFrame: DataFrame with NaNs filled.
+    """
+
+    df = df.fillna({
+        'orders_pre': 0,
+        'orders_post': 0,
+        'analytical_profit_pre': 0,
+        'analytical_profit_post': 0,
+    })
+
+    return df
+
+def drop_users_with_no_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Remove users with no pre or post period orders and profit data.
+
+    Args:
+        df (pd.DataFrame): Input DataFrame.
+
+    Returns:
+        pd.DataFrame: Filtered DataFrame.
+    """
+    
+    mask1 = (
+        (df['orders_pre'] == 0) &
+        (df['orders_post'] == 0) &
+        (df['analytical_profit_pre'] == 0) &
+        (df['analytical_profit_post'] == 0)
+    )
+
+    mask2 = (
+        (df['orders_pre'].isna()) &
+        (df['orders_post'].isna()) &
+        (df['analytical_profit_pre'].isna()) &
+        (df['analytical_profit_post'].isna())
+    ) 
+
+    df = df[~(mask1 |   mask2)]
+    
+    return df
+
+def apply_cleanup(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Apply NaN filling and remove users with no data in both periods.
+
+    Args:
+        df (pd.DataFrame): Input DataFrame.
+
+    Returns:
+        pd.DataFrame: Cleaned DataFrame.
+    """
+
+    initial_rows = len(df)
+    data_cleaned = fill_nans(df)
+    raw_data_final = drop_users_with_no_data(data_cleaned)
+    final_rows = len(raw_data_final)
+
+    logger.info(f"apply_cleanup(): {initial_rows - final_rows} rows removed. Final dataset size: {final_rows}")
+
+    return raw_data_final
