aa53448d368fb3e9adabb3c62ec8e4fc384864dd
shazebasad-dh <shazeb.asad@deliveryhero.com>
2025-05-15 01:48:15 +0200

    added entities for which we need the marketing data for

--- pricing_performance_holdouts/pipelines/run_pipeline.py
+++ pricing_performance_holdouts/pipelines/run_pipeline.py
@@ -8,12 +8,18 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(
 def main():
     
     project_id = "logistics-customer-staging"
-    entities = ('FP_PK', 'PY_DO')
+    #entities = ('FP_PK', 'PY_DO')
     
+    entities = tuple(set(('FP_PK','PY_DO','PY_BO', 'FP_TW', 'PY_PY', 'DJ_CZ', 'PY_EC',
+    'MJM_AT' ,'PY_PE', 'PY_AR' ,'PY_GT','PY_SV' ,'FP_PH','PY_NI' ,'NP_HU' ,'FP_MM','EF_GR' ,
+    'AP_PA' ,'YS_TR', 'PY_UY' ,'OP_SE' ,'PY_CL' ,'FP_BD' ,'FP_SG' ,'FO_NO' ,'PY_CR', 'FP_LA',
+    'PY_HN', 'FP_MY' ,'FP_TH', 'FY_CY', 'PY_VE','PO_FI','TB_QA','TB_OM',
+    'TB_KW','TB_JO','TB_IQ','TB_BH','TB_AE','HS_SA','FP_HK','FP_KH','HF_EG')))
+
     year = 2025
 
     min_date = date(2025, 2,2)    
-    max_date = date(2025, 2, 3) 
+    #max_date = date(2025, 2, 15) 
 
     store_data_historically(
         project_id=project_id,
