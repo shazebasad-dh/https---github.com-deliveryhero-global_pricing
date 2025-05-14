b51ff7261ed0ae0e5cb86ad9424ae1582aae3170
shazebasad-dh <shazeb.asad@deliveryhero.com>
2025-05-15 00:15:10 +0200

    created a py file to run pipeline

---
+++ pricing_performance_holdouts/pipelines/run_pipeline.py
@@ -0,0 +1,29 @@
+import logging
+from datetime import date
+import sys
+from pipelines.historical_pipeline import store_data_historically
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+def main():
+    
+    project_id = "logistics-customer-staging"
+    entities = ('FP_PK', 'PY_DO')
+    
+    year = 2025
+
+    min_date = date(2025, 2,2)    
+    max_date = date(2025, 2, 3) 
+
+    store_data_historically(
+        project_id=project_id,
+        entities=entities,
+        year=year,
+        min_date=min_date,
+        max_date=max_date,
+        restaurant_flag='IN',
+        save_local=True
+    )
+
+if __name__ == "__main__":
+    main()
